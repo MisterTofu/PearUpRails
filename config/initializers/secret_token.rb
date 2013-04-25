bf7593bc9931f190c89ed64f212c43ba39874f68
Travis <tebesu@gmail.com>
2013-04-25 10:55:10 -1000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PearUp2::Application.config.secret_token = '97340a664e4f4cf1bbfaa87f3e6de307d5fc79f13ae727c88f3b1d29351873260d25205b7f9294222458604aa6d5af947936b27a6a3fdeb50b18c47a2f0e1760'
