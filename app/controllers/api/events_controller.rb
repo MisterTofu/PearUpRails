9a03eee835e20a80113936788240d544f23d6e0d
Travis E <tebesu@gmail.com>
2013-04-26 18:08:35 -1000

    Updated token and events controllers

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,7 +1,13 @@
+# Author: Travis A Ebesu
+# Date: 4/26/2013
+
+
 class Api::EventsController < ApplicationController
-# before_filter :authenticate_user!
+before_filter :authenticate_user!
 respond_to :json
 
+
+# Authorization is required prior to access
 
 #  activity    :text
 #  description :text
@@ -20,7 +26,8 @@ respond_to :json
 # 		Fix User Authentication 
 # 		Input filtering 
 
-
+# Date/Time format: 2000-01-01 01:00:00.
+# YEAR:MM:DD HH:MM:SS
 def view
 
 end
@@ -28,9 +35,10 @@ end
     
 def create
 	if request.format != :json
-		render :status=>406, :json=>{:message=>"The request must be json"}
+		render :status=>406, :json=>{ :message=> "The request must be json"}
 		return
 	end
+	
 	activity = params[:activity]
 	description = params[:description]
 	max_attend = params[:max]
@@ -38,15 +46,23 @@ def create
 	start_time = params[:start]
 	end_time = params[:end]
 	location = params[:location]
-	
-# 	@api_event
 
-	# Check for duplicates, based on date
-	# get host id
-	@event = Event.find_or_create_by_activity :activity => activity, :description => description, :max_attend => max_attend, :date => date, :time_start => start_time, :time_end => end_time, :location => location, :cur_attend => 1
+	@user = User.find_by_authentication_token(params[:auth_token])	
+	
+	# we found user?
+	if @user.nil?
+		render :json => {:message => "Token not found"}
+		return
+	end
+	
+	puts start_time
+	# TO DO:	Check if user is busy
+	# 			Check for duplicates, based on date
+	# Figure out how to parse dates/times
+	@event = Event.find_or_create_by_activity :activity => activity, :description => description, :max_attend => max_attend, :date => date, :time_start => start_time, :time_end => end_time, :location => location, :cur_attend => 1, :host_id => @user.email
 	# Check Event already exists by user, no duplicates and user not busy
 	
-	 render :status=>200, :json=>{:message=> "Successfully added event"}	
+	 render :status=>200, :json=>{:message=> "Successfully added event", :debug => @event}	
 end
 
 def destroy
